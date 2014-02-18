1f2d81840f014f7af0204326f9fc5a5db6dfab0d
Chris Nitsche <chrizzio@vapintar.com>
2014-02-18 17:27:59 +0100

    Ausgewählte Würfel werden nicht mit den Vorschlag mit einbezogen

--- www/js/core.js
+++ www/js/core.js
@@ -18,29 +18,8 @@ $(document).ready(function()
 			$(".dice-"+(key+1)).html(value);
 		});
 		
-		// show the opportunities
-		
-		var numberOfOnes = numberOf(1);
-		var numberOfTwos = numberOf(2);
-		var numberOfThrees = numberOf(3);
-		var numberOfFours = numberOf(4);
-		var numberOfFives = numberOf(5);
-		var numberOfSixes = numberOf(6); 		
-
-		var $possibilites = new Array();
-		
-		if(numberOfOnes == 3){
-			$possibilites.push("Drei einsen: " + "1000");	
-		}
-		if(numberOfOnes > 0 && numberOfOnes < 3){
-			$possibilites.push(numberOfOnes + " " + ((numberOfOnes > 1) ? "Einsen:" : "Eins:") + " " + 100*numberOfOnes + " Punkte");	
-		}
-		
-		
-	$.each( $possibilites, function( key, value ) {
-		$(".possibility").html("");
-		$(".possibility").html($(".possibility").html() + "<br />" + "<div class='btn btn-default possibility'>" + value + '</div>');
-	});
+		// Show Opporturnities
+		showOpporturnities();
 		
 		
 /* 		checkRules();  */
@@ -74,26 +53,62 @@ function rollDices() {
 }
 
 // random number from 1 - 6 
-function roll() { 
-		
+function roll() { 		
 	return 1 + Math.floor(Math.random() * 6);
 }
 	
 // Prevent selected dices from beeing rolled
 function blockSelectedDices(){
 	$.each( $points, function( key, value ) {
-		if(!$(".dice-"+(key+1)).hasClass("btn-info")){
+		if(!isDiceSelected(key+1)){
 			$points[key] = 0;
 		}
 	});
 }
 
+// Ermittelt die Anzahl, wie oft eine bestimmte Zahl gewürfelt wurde
 function numberOf( $number ){
 	var $count = 0;
 	$.each( $points, function( key, value ) {
-		if(value == $number){
+		if(value == $number && !isDiceSelected(key+1)){
 			$count++;
 		}
 	});
 	return $count;
+}
+
+function showOpporturnities() { 		// show the opportunities
+		
+		var numberOfOnes = numberOf(1);
+		var numberOfTwos = numberOf(2);
+		var numberOfThrees = numberOf(3);
+		var numberOfFours = numberOf(4);
+		var numberOfFives = numberOf(5);
+		var numberOfSixes = numberOf(6); 		
+
+		var $possibilites = new Array();
+		
+		if(numberOfOnes == 3){
+			$possibilites.push("Drei einsen: " + "1000");	
+		}
+		if(numberOfOnes > 0 && numberOfOnes < 3){
+			$possibilites.push(numberOfOnes + " " + ((numberOfOnes > 1) ? "Einsen:" : "Eins:") + " " + 100*numberOfOnes + " Punkte");	
+		}
+		
+		$(".possibility").html("");
+		$.each( $possibilites, function( key, value ) {			
+			$(".possibility").html($(".possibility").html() + "<br />" + "<div class='btn btn-default possibility'>" + value + '</div>');
+	});
+	
+	
+}
+
+
+
+function isDiceSelected($key) { 
+	if($(".dice-"+($key)).hasClass("btn-info")){
+		return true;
+	} else{
+		return false;
+	}	
 }
